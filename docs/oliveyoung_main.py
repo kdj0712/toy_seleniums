0795ce1a861d4b3eac2d327b75f1a98546bb0b11
DeokJae Kim <kdj0712@gmail.com>
2023-12-31 16:20:51 +0900

    comments

--- docs/oliveyoung_main.py
+++ docs/oliveyoung_main.py
@@ -1,25 +1,30 @@
 import oliveyoung_move as move
 import oliveyoung_item as item
-
+# oliveyoung_move.py에 있는 function들을 인용하고 이를 move이라는 이름으로 치환한다.
+# oliveyoung_item.py에 있는 function들을 인용하고 이를 item이라는 이름으로 치환한다.
 
 
 # 기본 function 형식 - stand-bying. 호출되었을 때만 기능.
 def main(): 
     try :
         uri="https://www.oliveyoung.co.kr/store/main/main.do?oy=0"
-        browser = move.getOliveYoung(uri)    #  실제 동작을 하는 function을 기재하여 둔다.
+        browser = move.getOliveYoung(uri) 
+        #매개 변수인 uri를 사용할 move 파일의 get_OliveYoung 펑션을 실행시킨 것을 browser라는 변수로 담는다.
         browser = move.go_nail_care(browser)
+        #매개 변수인 browser를 사용할 move 파일의 go_nail_care 펑션을 실행시킨 것을 browser라는 변수로 담는다.
         browser = move.roof(browser=browser)
+        #매개 변수인 browser를 사용할 move 파일의 roof 펑션을 실행시킨 것을 browser라는 변수로 담는다.
     except:
-        pass   # 업무 코드가 문제 발생 시 대처 코드
+        pass
     finally:
-        move.quitBrowser(browser)  # 언제나 수행되어야 할 업무
+        move.quitBrowser(browser)  
+        #매개 변수인 browser를 사용할 move 파일의 quitBrowser function을 호출해서 프로그램을 종료시킨다..
     return 0
 
 if __name__ == "__main__":
     try :
-        main()   # 업무 코드
+        main()   # main function을 실행시킨다.
     except:
-        pass   # 업무 코드가 문제 발생 시 대처 코드
+        pass  
     finally:
-        pass   # try나 except가 끝난 후 무조건 실행 코드
+        pass  
